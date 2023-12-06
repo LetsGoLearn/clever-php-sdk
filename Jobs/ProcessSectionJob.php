08c2a4525d886c1d586a8a51b98886c78cac5fe2
github <action@github.com>
2023-12-06 07:41:09 +0000

    Merge pull request #7839 from LetsGoLearn/release/7.1.1

--- Jobs/ProcessSectionJob.php
+++ Jobs/ProcessSectionJob.php
@@ -24,32 +24,155 @@ class ProcessSectionJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels, ProcessCleverUserTrait, Batchable;
 
+    /* Steps to Process a roster from Clever
+    / 1. Get Teacher or Fail
+    / 2. Get Site or Fail
+    / 3. Get Roster or Create / Set Metadata
+    / 4. Sync Students
+    / 5. Sync Teachers
+    */
+
     protected $cleverSection;
     protected $client;
     protected $schools;
-
+    protected $site;
+    protected $teacher;
+    protected $roster;
     public $tries = 1;
 
     public function __construct($cleverSection, $clientId)
     {
 
+        // Set the queue name
         $this->onQueue('clever_full_sync');
-        $cleverSection = json_decode($cleverSection);
-        $sectionData = json_decode(json_encode($cleverSection->data), true);
-        $cleverSection->data = $sectionData;
-        $this->cleverSection = $cleverSection;
+        
+        // Data sent from Clever, set the JSON Object for usage during processing.
+        $this->cleverSection = json_decode($cleverSection);
+        
+        // Set the client object to an int, is replaced by the actual client during processing
         $this->client = $clientId;
     }
 
     public function handle()
     {
         $this->client = Client::find($this->client);
-        $roster = $this->findOrCreateRoster($this->cleverSection);
-
-        $this->syncTeachersToRoster($roster, $this->cleverSection->data['teachers']);
-        $this->syncStudentsToRoster($roster, $this->cleverSection->data['students']);
+        $this->setSite($this->cleverSection->data->school);
+        $this->setPrimaryTeacher($this->cleverSection->data->teacher);
+        $this->setRoster($this->cleverSection->data->id);
+        $this->syncStudents($this->cleverSection->data->students);
+        $this->syncTeachers($this->cleverSection->data->teachers);
     }
 
+    private function setSite(string $siteCleverId) {
+    
+        // whereHas() is expensive but works, needs to be adjusted.
+        $this->site = Site::whereClientId($this->client->id)->whereHas('metadata', function ($q) use ($siteCleverId) {
+            $q->where('data->clever_id', $siteCleverId);
+        })->first();
+        
+        // If its null we should log it and move on. Shouldn't matter if we need to create a floating roster to sync later.
+        // This is a good solution for fewer processes later.
+        if ($this->site === null) {
+            $this->logMessage(['Clever', 'Missing Site', 'Roster Sync'], "Could not find the site with Clever Id: ". $siteCleverId . " | Client Id:" . $this->client->id);
+            return;
+        }
+    }
+    
+    private function setRoster(string $cleverRosterId) {
+        $roster = Roster::where('client_id', $this->client->id)
+            ->whereHas('metadata', function ($q) use ($cleverRosterId) {
+                $q->where('data->clever_id', $cleverRosterId);
+            })
+            ->with('metadata')  // This is optional, only if you want to eager-load metadata
+            ->first();
+        if ($roster === null) {
+            $roster = new Roster();
+            $roster->type_id = 1;
+        }
+        $roster->client_id = $this->client->id;
+        $roster->writeable = false;
+        $roster->user_id = $this->teacher->id;
+        $roster->site_id = $this->site->id;
+        $roster->deleted_at = null;
+        $roster->title = $this->cleverSection->data->name . ' (Clever)';
+        $roster->description = "Clever Sync Job";
+        $roster->save();
+
+        $roster->setMetadata($this->buildRosterMetadata($this->cleverSection));
+
+        $this->roster = $roster;
+    }
+
+    private function setPrimaryTeacher($cleverPrimaryTeacherId) {
+        $this->teacher = EloquentUser::whereClientId($this->client->id)->whereHas('metadata', function ($q) use ($cleverPrimaryTeacherId) {
+            $q->where('data->clever_id', $cleverPrimaryTeacherId);
+        })->first();
+    }
+    
+    private function syncTeachers($teacherCleverIds)
+    {
+        $teachers = EloquentUser::whereClientId($this->client->id)->whereHas('metadata', function ($q) use ($teacherCleverIds) {
+            $q->whereIn('data->clever_id', $teacherCleverIds);
+        })->get();
+
+        $teacherIds = $teachers->pluck('id')->toArray();
+
+        $this->roster->access()->attach($teacherIds);
+    }
+
+    private function syncStudents($studentCleverIds)
+    {
+        $students = EloquentUser::whereClientId($this->client->id)->whereHas('metadata', function ($q) use ($studentCleverIds) {
+            $q->whereIn('data->clever_id', $studentCleverIds);
+        })->get();
+
+        $studentIds = $students->pluck('id')->toArray();
+        $this->roster->users()->sync($studentIds);
+    }
+
+    public function buildRosterMetadata($section) {
+        $subject = (isset($section->data->subject)) ? $this->checkSubject($section->data->subject) : null;
+        $period = (isset($section->data->period)) ? $this->checkPeriod($section->data->period) : null;
+        $course = (isset($section->data->course) && isset($section->data->number)) ? $this->checkCourse($section->data->name, $section->data->number) : null;
+
+
+        $metadata = [
+            'subject_id' => $subject->id ?? null,
+            'course_id' => $course->id ?? null,
+            'period_id' => $period->id ?? null,
+            'sis_id' => $section->data->sis_id ?? null,
+            'clever_id' => $section->data->id ?? null,
+            'created_by' => 'Clever Process - Client Sync - CLI',
+            'clever_data' => $section->data,
+        ];
+
+        return $metadata;
+    }
+    
+    private function logMessage(array $tags, string $message) {
+        Log::info('['.Carbon::now()->toDateTimeString().'][Clever][NullUser] : ' . $message);
+    }
+
+    public function checkSubject($subject)
+    {
+        /* @noinspection PhpUndefinedMethodInspection */
+        return Subjects::firstOrCreate(['name' => $subject, 'slug' => strtolower($subject)]);
+    }
+
+    public function checkCourse($name, $slug)
+    {
+        /* @noinspection PhpUndefinedMethodInspection */
+        return Courses::firstOrCreate(['name' => $name, 'slug' => $slug]);
+    }
+
+    public function checkPeriod($name)
+    {
+        /* @noinspection PhpUndefinedMethodInspection */
+        return Periods::firstOrCreate(['name' => $name, 'slug' => strtolower($name)]);
+    }
+
+    // DEPRICATED
+    
     private function findOrCreateRoster($section)
     {
         $roster = null;
@@ -66,23 +189,13 @@ class ProcessSectionJob implements ShouldQueue
             throw new CleverNullUser('Teacher not found for section ' . $section->data['name'] . ' (' . $section->data['id'] . ')' . ' in client ' . $this->client->id);
         }
 
-        $roster = Roster::where('client_id', $this->client->id)
-            ->whereHas('metadata', function ($q) use ($section) {
-                $q->where('data->clever_id', $section->data['id']);
-            })
-            ->with('metadata')  // This is optional, only if you want to eager-load metadata
-            ->first();
 
-        if ($roster === null) {
-            $roster = new Roster();
-            $roster->type_id = 1;
-        }
 
         $site = Site::whereClientId($this->client->id)->whereHas('metadata', function ($q) use ($section) {
             $q->where('data->clever_id', $section->data['school']);
         })->first();
 
-        // Pulls Site ID from an Array of Sites collected
+        // Pulls Site ID from an Array of Sites collected Ok lets fix this
         $roster->site_id = $site->id;
         $roster->client_id = $this->client->id;
         $roster->writeable = false;
@@ -104,67 +217,6 @@ class ProcessSectionJob implements ShouldQueue
         $roster->setMetadata($this->buildRosterMetadata($section));
 
         return $roster;
-    }
-
-    public function buildRosterMetadata($section)
-    {
-        // Clever Data Start Dates
-
-        $subject = (isset($section->data['subject'])) ? $this->checkSubject($section->data['subject']) : null;
-        $period = (isset($section->data['period'])) ? $this->checkPeriod($section->data['period']) : null;
-        $course = (isset($section->data['course']) && isset($section->data['number'])) ? $this->checkCourse($section->data['name'], $section->data['number']) : null;
-
-
-        $metadata = [
-            'subject_id' => $subject->id ?? null,
-            'course_id' => $course->id ?? null,
-            'period_id' => $period->id ?? null,
-            'sis_id' => $section->data['sis_id'] ?? null,
-            'clever_id' => $section->data['id'] ?? null,
-            'created_by' => 'Clever Process - Client Sync - CLI',
-            'clever_data' => $section->data,
-        ];
-
-        return $metadata;
-    }
-
-    private function syncTeachersToRoster($roster, $teacherCleverIds)
-    {
-        $teachers = EloquentUser::whereClientId($this->client->id)->whereHas('metadata', function ($q) use ($teacherCleverIds) {
-            $q->whereIn('data->clever_id', $teacherCleverIds);
-        })->get();
-
-        $teacherIds = $teachers->pluck('id')->toArray();
-
-        $roster->access()->attach($teacherIds);
-    }
-
-    private function syncStudentsToRoster($roster, $studentCleverIds)
-    {
-        $students = EloquentUser::whereClientId($this->client->id)->whereHas('metadata', function ($q) use ($studentCleverIds) {
-            $q->whereIn('data->clever_id', $studentCleverIds);
-        })->get();
-
-        $studentIds = $students->pluck('id')->toArray();
-        $roster->users()->sync($studentIds);
-    }
-
-    public function checkSubject($subject)
-    {
-        /* @noinspection PhpUndefinedMethodInspection */
-        return Subjects::firstOrCreate(['name' => $subject, 'slug' => strtolower($subject)]);
-    }
-
-    public function checkCourse($name, $slug)
-    {
-        /* @noinspection PhpUndefinedMethodInspection */
-        return Courses::firstOrCreate(['name' => $name, 'slug' => $slug]);
-    }
-
-    public function checkPeriod($name)
-    {
-        /* @noinspection PhpUndefinedMethodInspection */
-        return Periods::firstOrCreate(['name' => $name, 'slug' => strtolower($name)]);
     }
 
 }
